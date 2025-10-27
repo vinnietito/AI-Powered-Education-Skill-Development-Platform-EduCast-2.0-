d70e185ea99c2c71923d425a163be52f54663596
vinnietito <vincentnjuguna69@gmail.com>
2025-10-27 16:11:41 +0300

    Initialize server setup and project dependencies

---
+++ server/config/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("DB connection error:", err.message);
+    process.exit(1);
+  }
+};
